593edec51b05b7ac12199e20622cf31c26c5652b
Julien Girard <julien@tymate.com>
2015-06-11 11:43:50 +0200

    add links

--- routes/home.js
+++ routes/home.js
@@ -10,4 +10,8 @@ router.get('/about', function(req, res, next) {
   res.render('index/about', { title: 'About', pageDescription: 'Here is e quick guide to get started with the Api.'})
 });
 
+router.get('/links', function(req, res, next) {
+  res.render('index/links', { title: 'Links', pageDescription: 'Here are some links to test the apis.'})
+});
+
 module.exports = router;

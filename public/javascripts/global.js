fb4fb7bffd4dbdbcdb49df85c3a35a6188a6f977
Julien Girard <julien@tymate.com>
2015-06-11 15:09:58 +0200

    setup notifs for later

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -51,5 +51,9 @@ $(function() {
     var methodId = $(this).attr("data-method-id");
     deleteMethod(methodId);
   })
-
-});
+// swal({
+//   title: 'Auto close alert!',
+//   text: 'I will close in 2 seconds.',
+//   timer: 2000,
+//   showConfirmButton: false });
+// });

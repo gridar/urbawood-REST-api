1f8a824988cb645a9cbca13526a189e5af79c930
Julien Girard <julien@tymate.com>
2015-06-11 11:43:38 +0200

    clean up code

--- routes/apis.js
+++ routes/apis.js
@@ -18,23 +18,15 @@ router.use(methodOverride(function(req, res){
 
 // route middleware to validate :id
 router.param('id', function(req, res, next, id) {
-  //find the ID in the Database
   mongoose.model('Api').findById(id, function (err, api) {
-    //if it isn't found, we are going to repond with 404
     if (err) {
-      console.log(id + ' was not found');
       res.status(404)
-      var err = new Error('Not Found');
       err.status = 404;
-      res.format({
-          html: function(){
-              next(err);
-           }
-      });
       return next(err)
     //if it is found we continue on
     } else {
       req.id = id;
+      req.api = api;
       next(); 
     } 
   });
@@ -45,7 +37,7 @@ router.route('/').get(function(req, res, next) {
 
   mongoose.model('Api').find({}, function (err, apis) {
     
-    if (error) return next(error);
+    if (err) return next(err);
     
     res.format({
       html: function(){
@@ -61,7 +53,8 @@ router.route('/').get(function(req, res, next) {
 
 /* POST a new api. */
 router.route('/').post(function(req, res) {
-    // Get values from POST request.
+
+  // Get values from POST request.
   var name = req.body.name;
   var host = req.body.host;
   var description = req.body.description;
@@ -73,17 +66,16 @@ router.route('/').post(function(req, res) {
     description : description,
     methods : methods
   }, function (err, api) {
-    if (err) {
-      res.send("There was a problem adding the api to the database.");
-    } else {
-      console.log('POST creating new api: ' + api);
-      res.format({
-        html: function(){
-          res.location("apis");
-          res.redirect("/apis");
-        }
-      });
-    }
+
+    if (err) return next(err);
+
+    res.format({
+      html: function(){
+        res.location("apis");
+        res.redirect("/apis");
+      }
+    });
+
   })
 });
 
@@ -94,22 +86,22 @@ router.get('/new', function(req, res) {
 
 /* SHOW a Api by ID. */
 router.route('/:id').get(function(req, res) {
-  
-  mongoose.model('Api').findById(req.id, function (err, api) {
-    
-    if (error) return next(error);
+  //get api from route middleware
+  var api = req.api;
 
-    mongoose.model('Method').find({
-        '_id': { $in: api.methods}
-    }, function(err, methods){
-      res.format({
-        html: function(){
-          res.render('apis/show', {
-            "api" : api,
-            "methods" : methods
-          });
-        }
-      });
+  //get all methods in api.methods
+  var Model = mongoose.model('Method');
+  Model.find({'_id': { $in: api.methods}}, function(err, methods){
+
+    if (err) return next(err);
+
+    res.format({
+      html: function(){
+        res.render('apis/show', {
+          "api" : api,
+          "methods" : methods
+        });
+      }
     });
 
   });
@@ -117,74 +109,63 @@ router.route('/:id').get(function(req, res) {
 
 /* GET the Api by Mongo ID */
 router.route('/:id/edit').get(function(req, res) {
-  
-  mongoose.model('Api').findById(req.id, function (err, api) {
-    
-    if (error) return next(error);
+  //get api from route middleware
+  var api = req.api;
 
-    res.format({
-      html: function(){
-         res.render('apis/edit', {
-          title: 'Api' + api._id,
-          "api" : api
-        });
-      }
-    });
-
+  res.format({
+    html: function(){
+       res.render('apis/edit', {
+        title: 'Api' + api._id,
+        "api" : api
+      });
+    }
   });
+
 })
 
-/* PUT to update a Api by ID */
+/* PUT to update an Api by ID */
 router.route('/:id/edit').put(function(req, res) {
-  // Get our REST or form values.
+  //get api from route middleware
+  var api = req.api;
+
   var name = req.body.name;
   var host = req.body.host;
   var description = req.body.description;
 
-  mongoose.model('Api').findById(req.id, function (err, api) {
-    api.update({
-      name : name,
-      host : host,
-      description : description
-    }, function (err, apiID) {
-      if (err) {
-        res.send("There was a problem updating the information to the database: " + err);
-      } 
-      else {
-        //HTML responds by going back to the page or you can be fancy and create a new view that shows a success page.
-        res.format({
-          html: function(){
-            res.redirect("/apis/" + api._id);
-          }
-        });
+  api.update(
+  {
+    name : name,
+    host : host,
+    description : description
+  }, function (err, apiID) {
+    if (err) return next(err);
+
+    res.format({
+      html: function(){
+        res.redirect("/apis/" + api._id);
       }
-    })
-  });
+    });
+    
+  })
 })
 
 /* DELETE a Api by ID. */
 router.delete('/:id/edit', function (req, res){
-  //find api by ID
-  console.log(req.id)
-  mongoose.model('Api').findById(req.id, function (err, api) {
-    if (err) {
-        return console.error(err);
-    } else {
-      //remove it from Mongo
-      api.remove(function (err, api) {
-        if (err) {
-            return console.error(err);
-        } else {
-          //Returning success messages saying it was deleted
-          console.log('DELETE removing ID: ' + api._id);
-          res.format({
-            html: function(){
-              res.redirect("/apis");
-            }
-          });
-        }
-      });
-    }
+  //get api from route middleware
+  var api = req.api;
+
+  //remove it from Mongo
+  api.remove(function (err, api) {
+
+    if (err) return next(err);
+    
+    //Returning success messages saying it was deleted
+    res.format({
+      html: function(){
+        res.redirect("/apis");
+      }
+    });
+
   });
 });
 
